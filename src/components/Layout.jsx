8ea0f978280582f2d8df2c58cd9695a6273b2ade
zeevosec <oneill.shane.h@gmail.com>
2019-11-11 18:28:21 -0500

    feat: more improvements

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import '../assets/fonts/fontello-771c82e0/css/fontello.css';
 import '../assets/css/main.css';
 
 class Layout extends React.Component {
